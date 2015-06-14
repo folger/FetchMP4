f585c1fa2a6d3e0b3f1201277a13bc6c5ca93895
folger <folger@originlab.com.cn>
2015-06-14 15:56:28 +0800

    make it clean

--- makebin.bat
+++ makebin.bat
@@ -1,2 +1,1 @@
-C:\Python33\Scripts\cxfreeze.bat FetchMp4.pyw --target-dir=D:\FetchMp4 --base-name=C:\Python33\^
-lib\site-packages\cx_Freeze\bases\Win32GUI.exe --icon=main.ico
+C:\Python33\Scripts\cxfreeze.bat FetchMp4.pyw --base-name=C:\Python33\lib\site-packages\cx_Freeze\bases\Win32GUI.exe --icon=main.ico
--- snapshot.lua
+++
@@ -1,5 +0,0 @@
-function main()
-	mSleep(1000);
-    snapshotScreen(string.format("/var/touchelf/scripts/screenshots/%s.bmp",os.time()));
-    mSleep(1000);
-end
